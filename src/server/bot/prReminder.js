b782e9b0b1448cfcca1c7b918730e9559db04d09
Thomas Andreo <thomas.andreo@fansentertainment.com>
2018-05-23 13:31:05 -0400

    Added reviewers names to PR reminder

--- src/server/bot/prReminder.js
+++ src/server/bot/prReminder.js
@@ -3,21 +3,35 @@ const { CONFIG } = require('./constants');
 const getPendingPullRequests = require('../github/getPendingPullRequests');
 
 
+const filterApprovedPrs = ({ reviews }) =>
+  !reviews.some(review => review.state === 'APPROVED');
+
+const makePrMsg = (pr) => {
+  let msg = [
+    `>>> *${pr.title}*`,
+    pr.html_url,
+  ];
+
+  if (pr.requested_reviewers.length) {
+    msg = msg.concat([
+      `Reviewers: ${pr.requested_reviewers.map(user => user.login).join(' ')}`,
+    ]);
+  }
+
+  return msg.join('\r');
+};
+
 module.exports = (octokit, bot) => {
   schedule.scheduleJob('30 8 * * 1-5', () => { // be aware of server TZ
     getPendingPullRequests(octokit).then(promises => Promise.all(promises).then((pendingPrs) => {
-      const prs = pendingPrs.filter(({ reviews }) =>
-        !reviews.some(review => review.state === 'APPROVED'));
+      const prs = pendingPrs.filter(filterApprovedPrs);
       const prCount = prs.length;
       if (prCount) {
         const msg = [
-          '<!channel> Hey guys!',
-          `There ${prCount === 1 ? 'is' : 'are'} *${prCount} available PR${prCount === 1 ? '' : 's'}* awaiting review on the Web Client.`,
-          'https://github.com/fansapp/ordering-web-app/pulls',
+          `<!channel> There ${prCount === 1 ? 'is' : 'are'} *${prCount} pending PR${prCount === 1 ? '' : 's'}* on the Web Client:`,
+          ...prs.map(pr => makePrMsg(pr)),
         ].join('\r');
         bot.postMessage(process.env.CODE_REVIEW_CHANNEL_ID, msg, CONFIG);
-      } else {
-        bot.postMessage(process.env.CODE_REVIEW_CHANNEL_ID, 'Nothing to report :tada:', CONFIG);
       }
     }));
   });
