4349c336f50092c801757debbe7bf4565c36c205
Thomas Andreo <thomas.andreo@fansentertainment.com>
2019-04-23 16:19:28 -0400

    Updated project list

--- src/server/scm/getPendingPullRequests.js
+++ src/server/scm/getPendingPullRequests.js
@@ -4,10 +4,12 @@ const getFullPr = require('./getFullPr');
 module.exports = async (bbkit) => {
   const owaPrs = await bbkit.pullRequests.getAll('ordering-web-app');
   const gmcPrs = await bbkit.pullRequests.getAll('merchant-center-web-app');
+  const onboardingPrs = await bbkit.pullRequests.getAll('onboarding-web-app');
 
   const prs = [
     ...(owaPrs.values || []),
     ...(gmcPrs.values || []),
+    ...(onboardingPrs.values || []),
   ];
 
   const calls = prs.map(async pr => getFullPr(pr, bbkit));
